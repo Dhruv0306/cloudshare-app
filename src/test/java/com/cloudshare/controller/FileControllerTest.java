9180134b2a37c6ab1feac3fc4bc35063ab1a0829
Dhruv Rakeshbhai Patel <dpatel5469@gmail.com>
2026-06-25 13:15:55 +0530

    Migrate deprecated @MockBean to @MockitoBean and clean up unused imports in FileControllerTest

--- src/test/java/com/cloudshare/controller/FileControllerTest.java
+++ src/test/java/com/cloudshare/controller/FileControllerTest.java
@@ -7,12 +7,11 @@ import com.cloudshare.service.FileService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.ByteArrayInputStream;
@@ -35,16 +34,16 @@ class FileControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private FileService fileService;
 
-    @MockBean
+    @MockitoBean
     private JwtTokenProvider jwtTokenProvider;
 
-    @MockBean
+    @MockitoBean
     private CustomUserDetailsService customUserDetailsService;
 
-    @MockBean(name = "securityRedisTemplate")
+    @MockitoBean(name = "securityRedisTemplate")
     private org.springframework.data.redis.core.StringRedisTemplate securityRedisTemplate;
 
     private com.cloudshare.security.UserPrincipal getMockPrincipal() {
