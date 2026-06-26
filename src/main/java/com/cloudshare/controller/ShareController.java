527fd881e6902a9a8e8878180f4ad3877db7b777
Dhruv Rakeshbhai Patel <dpatel5469@gmail.com>
2026-06-26 17:50:15 +0530

    refactor: return 201 Created for internal share creations

--- src/main/java/com/cloudshare/controller/ShareController.java
+++ src/main/java/com/cloudshare/controller/ShareController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -30,7 +31,7 @@ public class ShareController {
             HttpServletRequest servletRequest) {
         
         InternalShareResponse response = shareService.shareFileInternally(request, principal.getId(), getClientIp(servletRequest));
-        return ResponseEntity.ok(ApiResponse.success(response));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(response));
     }
 
     @PostMapping("/link")
